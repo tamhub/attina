7d053401cc1a48f128484f19b07c748404bfaa70
Omar Bahareth <omar@omar.engineer>
2018-11-21 15:25:45 +0300

    Optimize webpack config to not include unnecessary stuff

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,34 @@ module.exports = {
   entry: "./tamtina.js",
   output: {
     path: path.resolve("dist"),
-    filename: "bundle.js"
+    filename: "bundle.js",
+    libraryTarget: 'umd',
+    publicPath: '/dist/',
+    umdNamedDefine: true
+  },
+  node: {
+    net: 'empty',
+    tls: 'empty',
+    dns: 'empty'
   },
   module: {
     loaders: [
       { test: /\.js$/, loader: "babel-loader", exclude: /node_modules/ }
     ]
-  }
+  },
+  externals: {
+    // Don't bundle react or react-dom
+    react: {
+        commonjs: "react",
+        commonjs2: "react",
+        amd: "React",
+        root: "React"
+    },
+    "react-dom": {
+        commonjs: "react-dom",
+        commonjs2: "react-dom",
+        amd: "ReactDOM",
+        root: "ReactDOM"
+    }
+}
 };
