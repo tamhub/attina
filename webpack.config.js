61830cc9f8ae6cf274f794139b2f28bcf36ef800
Omar Bahareth <omar@omar.engineer>
2018-11-21 14:57:27 +0300

    Rename docs/ to dist/ & use webpack bundle as entrypoint

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.resolve("docs"),
+    path: path.resolve("dist"),
     filename: "bundle.js"
   },
   module: {
