52ff90d89d24416bcb53384fdb3a6dcc1163cb59
Omar Bahareth <omar@omar.engineer>
2018-12-12 15:26:15 +0300

    Resolve node_modules when bundling and include mermaid in bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,29 @@
 import { uglify } from 'rollup-plugin-uglify'
 import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+import cjs from 'rollup-plugin-commonjs'
 
 const config = {
   input: './tamtina.js',
   external: ['react', 'react-dom'],
   plugins: [
+    cjs({
+      include: [
+        'node_modules/mermaid/dist/mermaid.core.js'
+      ]
+    }),
+    resolve(),
     babel({
-        exclude: "node_modules/**"
+      exclude: "node_modules/**"
     }),
     uglify()
   ],
   output: {
-      format: 'umd',
-      name: 'tamtina',
-      globals: {
-        react: "React",
-        mermaid: "mermaid"
-      }
+    format: 'umd',
+    name: 'tamtina',
+    globals: {
+      react: "React"
+    }
   }
 }
 export default config
